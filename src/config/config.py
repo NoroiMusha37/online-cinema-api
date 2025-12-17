7a5e6f65091d6238df081124ea2652be6b034620
NoroiMusha37 <vo.melnyk37@gmail.com>
2025-12-17 17:51:47 +0200

    add user, profile and token crud

--- src/config/config.py
+++ src/config/config.py
@@ -17,6 +17,8 @@ class Settings(BaseSettings):
     JWT_ENCODING_ALGORITHM: str = "HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
     REFRESH_TOKEN_EXPIRE_DAYS: int = 7
+    ACCOUNT_ACTIVATION_HOURS: int = 24
+    PASSWORD_RESET_TOKEN_EXPIRE_MINUTES: int = 30
 
 
 settings = Settings()
---
+++ src/crud/user.py
@@ -0,0 +1,94 @@
+from fastapi import HTTPException
+from pydantic import EmailStr
+from sqlalchemy import select, update
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from src.config.security import hash_password
+from src.models.user import User, UserGroup, UserGroupEnum, UserProfile
+from src.schemas.user import UserCreate, UserProfileCreate, UserProfileUpdate, UserUpdateAdmin
+
+
+async def get_user_by_email(
+        email: EmailStr, session: AsyncSession
+) -> User | None:
+    result = await session.execute(select(User).where(User.email == email))
+    return result.scalar_one_or_none()
+
+
+async def get_user_by_id(user_id: int, session: AsyncSession) -> User | None:
+    result = await session.execute(select(User).where(User.id == user_id))
+    return result.scalar_one_or_none()
+
+
+async def create_user(user_in: UserCreate, session: AsyncSession) -> User:
+    if await get_user_by_email(user_in.email, session):
+        raise HTTPException(
+            status_code=400, detail="Email already registered"
+        )
+
+    result = await session.execute(select(UserGroup)
+                                   .where(UserGroup.name == "USER"))
+    group = result.scalar_one_or_none()
+    if not group:
+        group = UserGroup(name=UserGroupEnum.USER)
+        session.add(group)
+        await session.flush()
+
+    user = User(
+        email=user_in.email,
+        hashed_password=hash_password(user_in.password),
+        group_id=group.id
+    )
+
+    user.profile = UserProfile(
+        first_name="",
+        last_name="",
+    )
+
+    session.add(user)
+    await session.commit()
+    await session.refresh(user)
+
+    return user
+
+
+async def get_profile_by_user_id(
+        user_id: int, session: AsyncSession
+) -> UserProfile | None:
+    result = await session.execute(select(UserProfile)
+                                   .where(UserProfile.user_id == user_id))
+    return result.scalar_one_or_none()
+
+
+async def update_profile(
+        user_id: int, profile_in: UserProfileUpdate, session: AsyncSession
+) -> UserProfile | None:
+    update_data = profile_in.model_dump(exclude_unset=True)
+    if not update_data:
+        return await get_profile_by_user_id(user_id, session)
+
+    await session.execute(update(UserProfile)
+                          .where(UserProfile.user_id == user_id)
+                          .values(**update_data)
+                          .execution_options(synchronize_session="fetch")
+                          )
+    await session.commit()
+
+    return await get_profile_by_user_id(user_id, session)
+
+
+async def update_user_admin(
+        user_id: int, user_in: UserUpdateAdmin, session: AsyncSession
+) -> User | None:
+    update_data = user_in.model_dump(exclude_unset=True)
+    if not update_data:
+        return await get_user_by_id(user_id, session)
+
+    await session.execute(update(User)
+                          .where(User.id == user_id)
+                          .values(**update_data)
+                          .execution_options(synchronize_session="fetch")
+                          )
+
+    await session.commit()
+    return await get_user_by_id(user_id, session)
