eaaa2e3addd3434587e7a72681054efa6c9317eb
NoroiMusha37 <vo.melnyk37@gmail.com>
2025-12-17 22:03:17 +0200

    add routers to main.py

--- main.py
+++ main.py
@@ -1,13 +1,9 @@
 from fastapi import FastAPI
 
+from src.routers import auth, users, admin
+
 app = FastAPI()
 
-
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
-
-
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+app.include_router(auth.router)
+app.include_router(users.router)
+app.include_router(admin.router)
