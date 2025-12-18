a88cde74ca6dfaa9eeb28415000195d2fa4936a2
NoroiMusha37 <vo.melnyk37@gmail.com>
2025-12-18 19:53:45 +0200

    add movie-related models

--- alembic/env.py
+++ alembic/env.py
@@ -10,6 +10,7 @@ from alembic import context
 from src.core.config import settings
 from src.core.database import Base
 from src.models.user import *
+from src.models.movie import *
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
---
+++ src/models/movie.py
@@ -0,0 +1,101 @@
+from decimal import Decimal
+from typing import Optional, List
+import uuid
+
+from sqlalchemy import ForeignKey, Table, Column, Numeric, UniqueConstraint
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+from src.core.database import Base
+
+
+movie_genres = Table(
+    "movie_genres",
+    Base.metadata,
+    Column("movie_id", ForeignKey("movies.id"), primary_key=True),
+    Column("genre_id", ForeignKey("genres.id"), primary_key=True),
+)
+
+movie_directors = Table(
+    "movie_directors",
+    Base.metadata,
+    Column("movie_id", ForeignKey("movies.id"), primary_key=True),
+    Column("director_id", ForeignKey("directors.id"), primary_key=True),
+)
+
+movie_stars = Table(
+    "movie_stars",
+    Base.metadata,
+    Column("movie_id", ForeignKey("movies.id"), primary_key=True),
+    Column("star_id", ForeignKey("stars.id"), primary_key=True),
+)
+
+
+class Genre(Base):
+    __tablename__ = "genres"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(unique=True, nullable=False)
+    movies: Mapped[List["Movie"]] = relationship(secondary=movie_genres, back_populates="genres")
+
+
+class Star(Base):
+    __tablename__ = "stars"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(unique=True, nullable=False)
+    movies: Mapped[List["Movie"]] = relationship(secondary=movie_stars, back_populates="stars")
+
+
+class Director(Base):
+    __tablename__ = "directors"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(unique=True, nullable=False)
+    movies: Mapped[List["Movie"]] = relationship(
+        secondary=movie_directors, back_populates="directors"
+    )
+
+
+class Certification(Base):
+    __tablename__ = "certifications"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(unique=True, nullable=False)
+    movies: Mapped[List["Movie"]] = relationship(
+        "Movie", back_populates="certification"
+    )
+
+
+class Movie(Base):
+    __tablename__ = "movies"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    uuid: Mapped[uuid.UUID]
+    name: Mapped[str]
+    year: Mapped[int]
+    time: Mapped[int]
+    imdb: Mapped[float]
+    votes: Mapped[int]
+    meta_score: Mapped[Optional[float]]
+    gross: Mapped[Optional[float]]
+    description: Mapped[str]
+    price: Mapped[Decimal] = mapped_column(Numeric(10, 2))
+    certification_id: Mapped[int] = mapped_column(ForeignKey(
+        "certifications.id"
+    ))
+    certification: Mapped[Certification] = relationship(
+        "Certification", back_populates="movies"
+    )
+    genres: Mapped[List[Genre]] = relationship(
+        secondary=movie_genres, back_populates="movies"
+    )
+    directors: Mapped[List[Director]] = relationship(
+        secondary=movie_directors, back_populates="movies"
+    )
+    stars: Mapped[List[Star]] = relationship(
+        secondary=movie_stars, back_populates="movies"
+    )
+
+    __table_args__ = (
+        UniqueConstraint(
+            "name",
+            "year",
+            "time",
+            name="uq_together_name_year_time"
+        ),
+    )
